5fffa8ab6abe8da4b3f7a62e7af255e762454211
dokinho <doks7081@live.com>
2024-05-31 18:32:09 +0200

    Refactor tooltips + set tooltip for all icons(apps)

--- assets/js/tooltips.js
+++
@@ -1,9 +0,0 @@
-const tooltipElements = document.querySelectorAll("[data-tooltip]");
-
-tooltipElements.forEach(tooltipElement => {
-  const boundingRect = tooltipElement.getBoundingClientRect();
-  const windowWidth = document.body.clientWidth;
-  const diff = windowWidth - boundingRect.x;
-
-  if (diff < 60) tooltipElement.style.setProperty("--left", "-200%");
-});
