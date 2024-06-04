1d5882b00b9e92d802a644092bddeebaa3f72f48
dborilovic7 <dborilovic7@users.noreply.github.com>
2024-06-04 09:22:00 +0000

    deploy: 6ad21a4bb28a01e79155cba2db85b88f368ec069

---
+++ assets/js/windows.js
@@ -0,0 +1,48 @@
+function setupWindow(id) {
+  const openBtn = document.getElementById(`open-btn${id}`);
+  const dialog = document.getElementById(`window${id}`);
+  const topbar = document.getElementById(`topbar${id}`);
+  // const topbarMinimizeBtn = document.getElementById(`topbar__actions-minimize${id}`);
+  // const topbarMaximizeBtn = document.getElementById(`topbar__actions-maximize${id}`);
+  const topbarCloseBtn = document.getElementById(`topbar__actions-close${id}`);
+  const draggableElementIds = [
+    `topbar${id}`,
+    `topbar__info${id}`,
+    `topbar__info-icon${id}`,
+    `topbar__info-name${id}`
+  ];
+  let offsetX, offsetY;
+
+  openBtn && openBtn.addEventListener("click", () => dialog.show());
+  topbarCloseBtn.addEventListener("click", () => dialog.close());
+  
+  topbar.addEventListener("mousedown", onMouseDown);
+  function onMouseDown(e) {
+    if (!draggableElementIds.includes(e.target.id)) return;
+
+    offsetX = e.offsetX;
+    offsetY = e.offsetY;
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  }
+
+  function onMouseMove(e) {
+    dialog.style.left = `${e.clientX - offsetX}px`;
+    dialog.style.top = `${e.clientY - offsetY}px`;
+  }
+
+  function onMouseUp(e) {
+    document.removeEventListener("mousemove", onMouseMove);
+
+    const topbarHalfWidth = topbar.scrollWidth / 2;
+    if (dialog.offsetLeft + topbarHalfWidth < 0) dialog.style.left = `-${topbarHalfWidth}px`;
+    else if (dialog.offsetLeft + topbarHalfWidth > window.innerWidth)
+      dialog.style.left = `${window.innerWidth - topbarHalfWidth}px`;
+
+    const topbarHeight = topbar.scrollHeight;
+    if (dialog.offsetTop < 0) dialog.style.top = "0px";
+    else if (dialog.offsetTop + topbarHeight > window.innerHeight)
+      dialog.style.top = `${window.innerHeight - topbarHeight}px`;
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.{md,html}',
+    './*.{md,html}'
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: []
+}
