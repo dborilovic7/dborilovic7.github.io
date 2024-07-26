276dc2f44db51c85b6f19963dab5ae66432b1b11
dokinho <doks7081@live.com>
2024-07-26 15:57:00 +0200

    Update colors / small restyle

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,24 @@ module.exports = {
     './*.{md,html}'
   ],
   theme: {
-    theme: {
-      extend: {},
-    },
+    extend: {
+      colors: {
+        'cerulean': {
+          DEFAULT: '#0CB5DF',
+          50: '#A8EAFA',
+          100: '#94E5F9',
+          200: '#6EDCF7',
+          300: '#47D2F5',
+          400: '#20C9F3',
+          500: '#0CB5DF',
+          600: '#0991B3',
+          700: '#076E88',
+          800: '#054B5C',
+          900: '#032730',
+          950: '#01161B'
+        }
+      }
+    }
   },
   plugins: []
 }
