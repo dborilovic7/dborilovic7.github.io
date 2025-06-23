60594d9e594b6be08072746f77f2e8587c73af59
Dominik Borilović <doks7081@live.com>
2025-06-23 19:06:00 +0200

    Fix a bug: Menu not closing when clicking its items

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,16 @@
 const menu = document.getElementById("menu");
 const menuBtn = document.getElementById("menu-btn");
+const menuItemBtns = document.querySelectorAll(".menu-item-btn");
 const taskbar = document.getElementById("taskbar");
 
 menuBtn.addEventListener("click", () => {
   menu.open ? menu.close() : menu.show();
+});
+
+menuItemBtns.forEach(menuItemBtn => {
+  menuItemBtn.addEventListener("click", () => {
+    menu.close();
+  });
 });
 
 document.addEventListener("pointerdown", e => {
